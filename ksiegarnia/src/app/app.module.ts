afa6c15e86171d8b878ca63a125ffe7315311754
Wojciech Bodnar <a-baltrukiewicz@o2.pl>
2017-10-18 15:45:03 +0200

    new components

--- ksiegarnia/src/app/app.module.ts
+++ ksiegarnia/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
