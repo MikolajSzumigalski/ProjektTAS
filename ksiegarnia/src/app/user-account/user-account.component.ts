d718f461917ca1a4410cd5f15b46fd3620094150
Wojciech Bodnar <a-baltrukiewicz@o2.pl>
2017-10-19 13:02:07 +0200

    jakies szczegoly poprawki

--- ksiegarnia/src/app/user-account/user-account.component.ts
+++ ksiegarnia/src/app/user-account/user-account.component.ts
@@ -22,30 +22,6 @@ export class UserAccountComponent implements OnInit {
       price: 64,
       image:'https://static.intelimedia.pl/sub/_bc25045.jpg',
       describtion:"Świat Erdas wciąż jest w niebezpieczeństwie Conor, Abeke, Meilin i Rollan starają się uratować świat przed starożytnym złem, ale więź łącząca zwierzoduchy z ich opiekunami jest coraz słabsza. Przyjaciele stają oko w oko z wrogiem, który posiada zdolność ...."
-    },
-    {
-      id:3,
-      name: 'Szubienicznik',
-      author: "Jacek Piekara",
-      price: 45,
-      image:'http://ecsmedia.pl/c/szubienicznik-tom-1-b-iext43249274.jpg',
-      describtion:"Świat Erdas wciąż jest w niebezpieczeństwie Conor, Abeke, Meilin i Rollan starają się uratować świat przed starożytnym złem, ale więź łącząca zwierzoduchy z ich opiekunami jest coraz słabsza. Przyjaciele stają oko w oko z wrogiem, który posiada zdolność ...."
-    },
-    {
-      id:4,
-      name: 'Szubienicznik',
-      author: "Jacek Piekara",
-      price: 45,
-      image:'http://ecsmedia.pl/c/szubienicznik-tom-1-b-iext43249274.jpg',
-      describtion:"Świat Erdas wciąż jest w niebezpieczeństwie Conor, Abeke, Meilin i Rollan starają się uratować świat przed starożytnym złem, ale więź łącząca zwierzoduchy z ich opiekunami jest coraz słabsza. Przyjaciele stają oko w oko z wrogiem, który posiada zdolność ...."
-    },
-    {
-      id:5,
-      name: 'Szubienicznik',
-      author: "Jacek Piekara",
-      price: 45,
-      image:'http://ecsmedia.pl/c/szubienicznik-tom-1-b-iext43249274.jpg',
-      describtion:"Świat Erdas wciąż jest w niebezpieczeństwie Conor, Abeke, Meilin i Rollan starają się uratować świat przed starożytnym złem, ale więź łącząca zwierzoduchy z ich opiekunami jest coraz słabsza. Przyjaciele stają oko w oko z wrogiem, który posiada zdolność ...."
     }
   ];
   constructor() { }
