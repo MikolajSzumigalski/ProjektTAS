dad726dde2cdbc50ccfd281e19a84b48962c3f49
Wojciech Bodnar <a-baltrukiewicz@o2.pl>
2017-10-19 14:32:43 +0200

    server framework express

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,25 @@
-var http = require('http');
+/*var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
+*/
+
+//Z frameworkiem EXPRESS
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (request, response) => {
+  response.send('Hello from Express!')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
