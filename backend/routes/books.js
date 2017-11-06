48cbc981b7b0b54ec455ef46a08e2f2a20fb6f74
MkolajSzumigalski <m.szumigalski@op.pl>
2017-11-06 01:53:26 +0100

    wyszukiwanie książki po id + zmiana nazewnictwa

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -4,8 +4,17 @@ var mongojs = require('mongojs');
 var db =  mongojs('mongodb://janusz:qwerty@ds125365.mlab.com:25365/uamtas', ['books']);
 
 //get all books
-router.get('/', function(req,res,next){
+router.get('/books', function(req,res,next){
     db.books.find(function(err, books){
+        if(err){
+            res.send(err);
+        }
+        res.json(books);
+    });
+});
+//get one book by id
+router.get('/book/:id', function(req, res, next){
+    db.books.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, books){
         if(err){
             res.send(err);
         }
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 8080
 
 var index = require('./routes/index');
-var tasks = require('./routes/tasks');
+var books = require('./routes/books');
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
-app.use('/api', tasks);
+app.use('/api', books);
 app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
