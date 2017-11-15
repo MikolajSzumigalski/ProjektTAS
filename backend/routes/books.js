ac08e461846d64687edb1373bdefc76673076251
Wojciech Bodnar <a-baltrukiewicz@o2.pl>
2017-11-15 19:15:42 +0100

    pobieranie jako json z statusem 200

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -7,7 +7,9 @@ var db =  mongojs('mongodb://janusz:qwerty@ds125365.mlab.com:25365/uamtas', ['bo
 //get all books
 //app.use(bodyParser.urlencoded({ extended: false }))
 //app.use(bodyParser.json());
+
 router.use(bodyParser.urlencoded({extended: true}))
+router.use(bodyParser.json());
 router.get('/books', function(req,res,next){
     db.books.find(function(err, books){
         if(err){
@@ -16,15 +18,19 @@ router.get('/books', function(req,res,next){
 		//res.setHeader("status", 200)
         //console.log(JSON.stringify(books).substring(1, JSON.stringify(books).length-1));
 		//data = JSON.stringify(books).substring(1, JSON.stringify(books).length-1);
-		//res.json(books);
+		//res.json(books[0]);
 		//console.log(data);
-		//dane = '{"status":"200 OK","data":' + JSON.stringify(books)+ '}';
+		dane = '{"status":"200","data":' + JSON.stringify(books)+ '}';
 		//console.log(dane);
-		dane =  '{"status":"200 OK","data":'+JSON.stringify(books)+'}';
+		//dane =  '{"status":"200 OK","data":'+JSON(books)+'}';
 		//res.writeHead(200).json(books);
-		//res.status(200).json(books)
+		//console.log(JSON.parse(dane));
+		jsonik=JSON.parse(dane);
+		res.send(jsonik);
 		//res.send(dane);
-		res.send(dane);
+		//res.send(dane);
+
+        // finish the request
         //message: 'Welcome to the project-name api'
     //});
         //res.String(json(books)).substring(3,4);
